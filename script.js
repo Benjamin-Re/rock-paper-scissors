8ad2f7474e7f1e8a126c16d7a1052a26a036f8e8
Benjamin-Re <renker.benjamin@gmail.com>
2022-01-15 11:04:54 +0100

    Added computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello");
+function computerPlay(){
+    /* Randomly return "Rock", "Paper" or "Scissors"*/
+    // Make an array containing the options
+    const options = ["Rock", "Paper", "Scissors"];
+
+    // Generate a random number between 0 and 2
+    let randomNumber = Math.floor(Math.random()*3);
+
+    // Return array[randomNumber]
+    return options[randomNumber];
+}
